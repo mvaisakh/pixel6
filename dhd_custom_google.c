8d0bf74054e6ab1bfc1ce0f2161146b370b27f69
Roger Wang <wangroger@google.com>
2020-03-23 12:52:36 +0800

    bcmdhd: error handling enhancement
    In this commit, we enhance error handling when
    alloc reserved memory failed.
    
    Bug: 152166445
    Test: wifi is able to turn ON
    Change-Id: I3a5139089b4f88ed181414fbe7250cf9b31fd26c

--- dhd_custom_google.c
+++ dhd_custom_google.c
@@ -266,11 +266,12 @@ dhd_wlan_init(void)
 #endif /* CONFIG_BCMDHD_OOB_HOST_WAKE */
 
 #ifdef CONFIG_BROADCOM_WIFI_RESERVED_MEM
-	  ret = dhd_init_wlan_mem();
-	  if (ret < 0) {
-		  printk(KERN_ERR "%s: failed to alloc reserved memory,"
-			  " ret=%d\n", __FUNCTION__, ret);
-	  }
+	ret = dhd_init_wlan_mem();
+	if (ret < 0) {
+		printk(KERN_ERR "%s: failed to alloc reserved memory,"
+			" ret=%d\n", __FUNCTION__, ret);
+		goto fail;
+	}
 #endif /* CONFIG_BROADCOM_WIFI_RESERVED_MEM */
 
 fail:
