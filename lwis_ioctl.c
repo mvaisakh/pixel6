6aff0da111ac27ee3cb423b9ec7babed0fce27ef
Edmond Chung <edmondchung@google.com>
2020-03-25 09:02:16 -0700

    IOCTL: Temporarily allow buffer enrollment when disabled
    This is just a temporary workaround as we wait for a proper fix to
    ensure buffer enrollment in enabled state.
    
    Bug: 151516319
    Signed-off-by: Edmond Chung <edmondchung@google.com>
    Change-Id: Ibb419ddf73a783d77ef719969dd0f245e777c507

--- lwis_ioctl.c
+++ lwis_ioctl.c
@@ -937,10 +937,13 @@ int lwis_ioctl_handler(struct lwis_client *lwis_client, unsigned int type,
 	mutex_lock(&lwis_dev->client_lock);
 	device_disabled = (lwis_dev->enabled == 0);
 	mutex_unlock(&lwis_dev->client_lock);
+	/* Buffer dis/enroll is added here temporarily. Will need a proper
+	   fix to ensure buffer enrollment when device is enabled. */
 	if (lwis_dev->type != DEVICE_TYPE_TOP && device_disabled &&
 	    type != LWIS_GET_DEVICE_INFO && type != LWIS_DEVICE_ENABLE &&
 	    type != LWIS_EVENT_CONTROL_GET && type != LWIS_TIME_QUERY &&
-	    type != LWIS_EVENT_DEQUEUE) {
+	    type != LWIS_EVENT_DEQUEUE && type != LWIS_BUFFER_ENROLL &&
+	    type != LWIS_BUFFER_DISENROLL) {
 		ret = -EBADFD;
 		pr_err("Unsupported IOCTL on disabled device.\n");
 		return ret;
