7d3d739555446fc96065680a7a1435d84346b5c9
Long Ling <longling@google.com>
2020-09-24 10:14:08 -0700

    include/drm/exynos: Add parameters for underrun_lp_ref
    These parameters are used to calculate underrun_lp_ref.
    
    Change-Id: I7cc8713b9eb4cc5acba119c0305af4a5b2c39903
    Signed-off-by: Long Ling <longling@google.com>

--- include/drm/exynos_display_common.h
+++ include/drm/exynos_display_common.h
@@ -26,6 +26,15 @@ struct exynos_display_dsc {
 	unsigned int slice_height;
 };
 
+struct exynos_display_underrun_param {
+	/* te idle (us) to calculate underrun_lp_ref */
+	unsigned int te_idle_us;
+	/* te variation (percentage) to calculate underrun_lp_ref */
+	unsigned int te_var;
+	/* display mode used to calculate underrun_lp_ref */
+	const struct drm_display_mode *ref_mode;
+};
+
 struct exynos_display_mode {
 	/* DSI mode flags in drm_mipi_dsi.h */
 	unsigned long mode_flags;
@@ -33,6 +42,8 @@ struct exynos_display_mode {
 	unsigned int vblank_usec;
 	unsigned int bpc;
 	struct exynos_display_dsc dsc;
+	/* parameters to calculate underrun_lp_ref when hs_clock changes */
+	const struct exynos_display_underrun_param *underrun_param;
 };
 
 static inline const struct exynos_display_mode *drm_mode_to_exynos(
