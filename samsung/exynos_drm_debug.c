mvaisakh/pixel6

commit bfc2beade0dbc5390eec03c22954634ea722a355

drivers/exynos_drm: display mipi phy diag
Parse the phy diag settings from dts. Create debugfs nodes for each
phy diag item.

If dsim is in enabled status, restart dsim to apply user's input.
Otherwise user's input will be applied the next time dsim is enabled.

User's input will override the phy default settings before pll is enabled.

Bug: 139663511
Change-Id: Ib00bccab0ee89c11e23d6690785bfe962536cc7f
Signed-off-by: Long Ling <longling@google.com>

samsung/exynos_drm_debug.c
@@ -720,4 +720,113 @@ int dpu_itmon_notifier(struct notifier_block *nb, unsigned long act, void *data)
 
 	return NOTIFY_DONE;
 }
+
+#endif
+
+#ifdef CONFIG_DEBUG_FS
+static int dphy_diag_text_show(struct seq_file *m, void *p)
+{
+      seq_printf(m, "%s\n", m->private);
+      return 0;
+}
+
+DEFINE_SHOW_ATTRIBUTE(dphy_diag_text);
+
+static ssize_t dphy_diag_reg_write(struct file *file, const char *user_buf,
+			      size_t count, loff_t *f_pos)
+{
+	int ret;
+	uint32_t val;
+	struct seq_file *m = file->private_data;
+	struct dsim_dphy_diag *diag = m->private;
+
+	ret = kstrtou32_from_user(user_buf, count, 0, &val);
+	if (ret)
+		return ret;
+
+	ret = dsim_dphy_diag_set_reg(diag->private, diag, val);
+	if (ret)
+		return ret;
+
+	return count;
+}
+
+static int dphy_diag_reg_show(struct seq_file *m, void *data)
+{
+	struct dsim_dphy_diag *diag = m->private;
+	uint32_t regs[MAX_DIAG_REG_NUM];
+	uint32_t ix;
+	int ret;
+
+	ret = dsim_dphy_diag_get_reg(diag->private, diag, regs);
+
+	if (ret == 0) {
+		for (ix = 0; ix < diag->num_reg; ++ix)
+			seq_printf(m, "%d ", regs[ix]);
+		seq_puts(m, "\n");
+	}
+
+	return ret;
+}
+
+static int dphy_diag_reg_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, dphy_diag_reg_show, inode->i_private);
+}
+
+static const struct file_operations dphy_diag_reg_fops = {
+	.owner = THIS_MODULE,
+	.open = dphy_diag_reg_open,
+	.write = dphy_diag_reg_write,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+void dsim_diag_create_debugfs(struct dsim_device *dsim) {
+	struct dentry *dent_dphy;
+	struct dentry *dent_diag;
+	struct dsim_dphy_diag *diag;
+	char dir_name[32];
+	int ix;
+
+	scnprintf(dir_name, sizeof(dir_name), "dsim%d", dsim->id);
+	dsim->debugfs_entry = debugfs_create_dir(
+		dir_name, dsim->encoder.dev->primary->debugfs_root);
+	if (!dsim->debugfs_entry) {
+		pr_warn("%s: failed to create %s\n", __func__, dir_name);
+		return;
+	}
+
+	if (dsim->config.num_dphy_diags == 0)
+		return;
+
+	dent_dphy = debugfs_create_dir("dphy", dsim->debugfs_entry);
+	if (!dent_dphy) {
+		pr_warn("%s: failed to create %s\n", __func__, dir_name);
+		return;
+	}
+
+	for (ix = 0; ix < dsim->config.num_dphy_diags; ++ix) {
+		diag = &dsim->config.dphy_diags[ix];
+		dent_diag = debugfs_create_dir(diag->name, dent_dphy);
+		if (!dent_diag) {
+			pr_warn("%s: failed to create %s\n", __func__,
+				diag->name);
+			continue;
+		}
+		debugfs_create_file("desc", 0400, dent_diag, (void *)diag->desc,
+				    &dphy_diag_text_fops);
+		debugfs_create_file("help", 0400, dent_diag, (void *)diag->help,
+				    &dphy_diag_text_fops);
+		diag->private = dsim;
+		debugfs_create_file("value", diag->read_only ? 0400 : 0600,
+				    dent_diag, diag, &dphy_diag_reg_fops);
+	}
+}
+
+void dsim_diag_remove_debugfs(struct dsim_device *dsim) {
+	debugfs_remove_recursive(dsim->debugfs_entry);
+	dsim->debugfs_entry = NULL;
+}
 #endif

samsung/cal_9845/dsim_reg.c
@@ -47,6 +47,10 @@ static struct cal_regs_desc regs_desc[REGS_DSIM_TYPE_MAX][MAX_DSI_CNT];
 	(&regs_desc[REGS_DSIM_PHY_BIAS][id])
 #define dsim_phy_extra_write(id, offset, val)		\
 	cal_write(dphy_bias_regs_desc(id), offset, val)
+#define dsim_phy_extra_read_mask(id, offset, mask)       \
+	cal_read_mask(dphy_bias_regs_desc(id), offset, mask)
+#define dsim_phy_extra_write_mask(id, offset, val, mask) \
+	cal_write_mask(dphy_bias_regs_desc(id), offset, val, mask);
 
 #define sys_regs_desc(id)				\
 	(&regs_desc[REGS_DSIM_SYS][id])
@@ -502,7 +506,7 @@ static void dsim_reg_set_dphy_timing_values(u32 id,
 	}
 }
 
-static void dsim_reg_set_dphy_param_dither(u32 id, struct stdphy_pms *dphy_pms)
+static void dsim_reg_set_dphy_param_dither(u32 id, const struct stdphy_pms *dphy_pms)
 {
 	u32 val, mask;
 
@@ -1622,6 +1626,37 @@ static void dsim_reg_set_config(u32 id, struct dsim_reg_config *config,
 	}
 }
 
+static void dsim_reg_diag_apply(u32 id, u32 num_dphy_diag,
+				struct dsim_dphy_diag *dphy_diags)
+{
+	int ret;
+	u32 diag_ix, reg_ix, mask;
+	struct dsim_dphy_diag *diag;
+
+	for (diag_ix = 0; diag_ix < num_dphy_diag; ++diag_ix) {
+		diag = &dphy_diags[diag_ix];
+		if (!diag->override)
+			continue;
+		ret = dsim_dphy_diag_mask_from_range(diag->bit_start,
+						     diag->bit_end, &mask);
+		if (ret < 0)
+			continue;
+		if (diag->reg_base == REGS_DSIM_PHY) {
+			for (reg_ix = 0; reg_ix < diag->num_reg; ++reg_ix) {
+				dsim_phy_write_mask(id,
+						    diag->reg_offset[reg_ix],
+						    diag->user_value, mask);
+			}
+		} else if (diag->reg_base == REGS_DSIM_PHY_BIAS) {
+			for (reg_ix = 0; reg_ix < diag->num_reg; ++reg_ix) {
+				dsim_phy_extra_write_mask(
+					id, diag->reg_offset[reg_ix],
+					diag->user_value, mask);
+			}
+		}
+	}
+}
+
 /*
  * configure and set DPHY PLL, byte clock, escape clock and hs clock
  *	- PMS value have to be optained by using PMS Gen.
@@ -1640,13 +1675,15 @@ static void dsim_reg_set_config(u32 id, struct dsim_reg_config *config,
  *		in : requested escape clock. out : calculated escape clock
  *	- word_clk : out parameter. byte clock = hs clock / 16
  */
-static int dsim_reg_set_clocks(u32 id, struct dsim_clks *clks,
-		struct stdphy_pms *dphy_pms, u32 en)
+
+static int dsim_reg_set_clocks(u32 id, const struct dsim_reg_config *config,
+			       struct dsim_clks *clks, u32 en)
 {
 	unsigned int esc_div;
 	struct dsim_pll_param pll;
 	struct dphy_timing_value t;
 	u32 pll_lock_cnt;
+	const struct stdphy_pms *dphy_pms;
 	int ret = 0;
 	u32 hsmode = 0;
 #ifdef DPDN_INV_SWAP
@@ -1654,6 +1691,11 @@ static int dsim_reg_set_clocks(u32 id, struct dsim_clks *clks,
 #endif
 
 	if (en) {
+		if (!config) {
+			cal_log_err(id, "%s invalid config (null)\n", __func__);
+			return -EINVAL;
+		}
+
 		/*
 		 * Do not need to set clocks related with PLL,
 		 * if DPHY_PLL is already stabled because of LCD_ON_UBOOT.
@@ -1668,6 +1710,7 @@ static int dsim_reg_set_clocks(u32 id, struct dsim_clks *clks,
 		 * PMS value has to be optained by PMS calculation tool
 		 * released to customer
 		 */
+		dphy_pms = &config->dphy_pms;
 		pll.p = dphy_pms->p;
 		pll.m = dphy_pms->m;
 		pll.s = dphy_pms->s;
@@ -1757,6 +1800,7 @@ static int dsim_reg_set_clocks(u32 id, struct dsim_clks *clks,
 #ifdef DPHY_LOOP
 		dsim_reg_set_dphy_loop_test(id);
 #endif
+		dsim_reg_diag_apply(id, config->num_dphy_diags, config->dphy_diags);
 		/* enable PLL */
 		ret = dsim_reg_enable_pll(id, 1);
 	} else {
@@ -1765,7 +1809,7 @@ static int dsim_reg_set_clocks(u32 id, struct dsim_clks *clks,
 		dsim_reg_set_esc_clk_prescaler(id, 0, 0xff);
 
 		/* check dither sequence */
-		if (dphy_pms && dphy_pms->dither_en)
+		if (config != NULL && config->dphy_pms.dither_en)
 			dsim_reg_set_dphy_dither_en(id, 0);
 
 		dsim_reg_enable_pll(id, 0);
@@ -2032,8 +2076,7 @@ void dsim_reg_init(u32 id, struct dsim_reg_config *config,
 #endif
 
 #if !defined(CONFIG_BOARD_EMULATOR)
-	dsim_reg_set_clocks(id, clks, &config->dphy_pms, 1);
-
+	dsim_reg_set_clocks(id, config, clks, 1);
 	dsim_reg_set_lanes_dphy(id, lanes, true);
 	dpu_sysreg_dphy_reset(id, 1); /* Release DPHY reset */
 #endif
@@ -2140,7 +2183,6 @@ int dsim_reg_stop_and_enter_ulps(u32 id, u32 ddi_type, u32 lanes)
 	/* 3.3 off DPHY */
 	dsim_reg_set_lanes_dphy(id, lanes, false);
 	dsim_reg_set_clocks(id, NULL, NULL, 0);
-
 	/* 3.4 sw reset */
 	dsim_reg_sw_reset(id);
 
@@ -2437,4 +2479,26 @@ void __dsim_dump(u32 id, struct dsim_regs *regs)
 
 	/* restore to avoid size mismatch (possible config error at DECON) */
 	dsim_reg_enable_shadow_read(id, 1);
+}
+
+int dsim_dphy_diag_mask_from_range(uint8_t start, uint8_t end, uint32_t *mask)
+{
+	if (start > end || end >= 32) {
+		pr_err("%s: invalid bit range [%u, %u]\n", __func__, start,
+		       end);
+		return -EINVAL;
+	}
+
+	*mask = (((uint32_t)1 << (end - start + 1)) - 1) << start;
+	return 0;
+}
+
+u32 diag_dsim_dphy_reg_read_mask(u32 id, u16 offset, u32 mask)
+{
+	return dsim_phy_read_mask(id, offset, mask);
+}
+
+u32 diag_dsim_dphy_extra_reg_read_mask(u32 id, u16 offset, u32 mask)
+{
+	return dsim_phy_extra_read_mask(id, offset, mask);
 }

samsung/cal_common/dsim_cal.h
@@ -152,6 +152,26 @@ struct dphy_timing_value {
 	unsigned int b_dphyctl;
 };
 
+/* Max registers number in one diag command: 1 clock + 4 data lanes */
+#define MAX_DIAG_REG_NUM 5
+
+struct dsim_dphy_diag {
+	const char *name;
+	const char *desc;
+	const char *help;
+	uint8_t num_reg;
+	/* REGS_DSIM_PHY or REGS_DSIM_PHY_BIAS */
+	uint8_t reg_base;
+	uint16_t reg_offset[MAX_DIAG_REG_NUM];
+	uint8_t bit_start;
+	uint8_t bit_end;
+	bool read_only;
+
+	bool override;
+	uint32_t user_value;
+	void *private;
+};
+
 struct dsim_reg_config {
 	struct dpu_panel_timing	p_timing;
 	enum dsim_panel_mode	mode;
@@ -163,6 +183,8 @@ struct dsim_reg_config {
 	unsigned int		mres_mode;
 	unsigned int		cmd_underrun_cnt[MAX_RES_NUMBER];
 	unsigned int		bpp;
+	unsigned int num_dphy_diags;
+	struct dsim_dphy_diag *dphy_diags;
 };
 
 void dsim_regs_desc_init(void __iomem *reg_base, const char *name,
@@ -212,4 +234,10 @@ void dsim_reg_set_dphy_freq_hopping(u32 id, u32 p, u32 m, u32 k, u32 en);
 
 /* DSIM SFR dump */
 void __dsim_dump(u32 id, struct dsim_regs *regs);
+
+/* For dphy diagnosis */
+u32 diag_dsim_dphy_reg_read_mask(u32 id, u16 offset, u32 mask);
+u32 diag_dsim_dphy_extra_reg_read_mask(u32 id, u16 offset, u32 mask);
+int dsim_dphy_diag_mask_from_range(uint8_t start, uint8_t end, uint32_t *mask);
+
 #endif /* __SAMSUNG_DSIM_CAL_H__ */

samsung/exynos_drm_dsim.c
@@ -582,6 +582,193 @@ getnode_fail:
 	return NULL;
 }
 
+static void dsim_restart(struct dsim_device *dsim)
+{
+	mutex_lock(&dsim->cmd_lock);
+	dsim_reg_stop(dsim->id, 0x1F);
+	disable_irq(dsim->irq);
+
+	dsim_reg_init(dsim->id, &dsim->config, &dsim->clk_param, true);
+	dsim_reg_start(dsim->id);
+	enable_irq(dsim->irq);
+	mutex_unlock(&dsim->cmd_lock);
+}
+
+#ifdef CONFIG_DEBUG_FS
+
+static int dsim_of_parse_diag(struct device_node *np, struct dsim_dphy_diag *diag)
+{
+        int count;
+        u8 bit_range[2];
+        const char *reg_base = NULL;
+
+        of_property_read_string(np, "reg-base", &reg_base);
+        if (!strcmp(reg_base, "dphy")) {
+                diag->reg_base = REGS_DSIM_PHY;
+        } else if (!strcmp(reg_base, "dphy-extra")) {
+                diag->reg_base = REGS_DSIM_PHY_BIAS;
+        } else {
+                pr_err("%s: invalid reg-base: %s\n", __func__, reg_base);
+                return -EINVAL;
+        }
+
+        of_property_read_string(np, "diag-name", &diag->name);
+        if (!diag->name || !diag->name[0]) {
+                pr_err("%s: empty diag-name\n", __func__);
+                return -EINVAL;
+        }
+
+        of_property_read_string(np, "desc", &diag->desc);
+        of_property_read_string(np, "help", &diag->help);
+
+        count = of_property_count_u16_elems(np, "reg-offset");
+        if (count <= 0 || count > MAX_DIAG_REG_NUM) {
+                pr_err("%s: wrong number of reg-offset: %d\n", __func__, count);
+                return -ERANGE;
+        }
+
+        if (of_property_read_u16_array(np, "reg-offset", diag->reg_offset, count) < 0) {
+                pr_err("%s: failed to read reg-offset\n", __func__);
+                return -EINVAL;
+        }
+        diag->num_reg = count;
+
+        if (of_property_read_u8_array(np, "bit-range", bit_range, 2) < 0) {
+                pr_err("%s: failed to read bit-range\n", __func__);
+                return -EINVAL;
+        }
+
+        if (bit_range[0] >= 32 || bit_range[1] >= 32) {
+                pr_err("%s: invalid bit range %d, %d\n", __func__, bit_range[0], bit_range[1]);
+                return -EINVAL;
+        }
+        if (bit_range[0] < bit_range[1]) {
+                diag->bit_start = bit_range[0];
+                diag->bit_end = bit_range[1];
+        } else {
+                diag->bit_start = bit_range[1];
+                diag->bit_end = bit_range[0];
+        }
+        diag->read_only = of_property_read_bool(np, "read_only");
+
+        return 0;
+}
+
+static void dsim_of_get_pll_diags(struct dsim_device *dsim)
+{
+	struct device_node *np, *entry;
+	struct device *dev = dsim->dev;
+        uint32_t index = 0;
+
+	np = of_parse_phandle(dev->of_node, "dphy_diag", 0);
+        dsim->config.num_dphy_diags = of_get_child_count(np);
+        if (dsim->config.num_dphy_diags == 0) {
+                goto nochild;
+        }
+
+	dsim->config.dphy_diags = devm_kzalloc(dsim->dev, sizeof(struct dsim_dphy_diag) *
+					dsim->config.num_dphy_diags, GFP_KERNEL);
+	if (!dsim->config.dphy_diags) {
+                dsim_warn(dsim, "%s: no memory for %u diag items\n",
+                          __func__, dsim->config.num_dphy_diags);
+                dsim->config.num_dphy_diags = 0;
+                goto nochild;
+        }
+
+        for_each_child_of_node(np, entry) {
+                if (index >= dsim->config.num_dphy_diags) {
+                      dsim_warn(dsim, "%s: diag parsing error with unexpected index %u\n",
+                                __func__, index);
+                      goto get_diag_fail;
+                }
+
+                if (dsim_of_parse_diag(entry, &dsim->config.dphy_diags[index]) < 0) {
+                      dsim_warn(dsim, "%s: diag parsing error for item %u\n",
+                                __func__, index);
+                      goto get_diag_fail;
+                }
+                ++index;
+        }
+        return;
+
+get_diag_fail:
+        dsim->config.num_dphy_diags = 0;
+        devm_kfree(dsim->dev, dsim->config.dphy_diags);
+        dsim->config.dphy_diags = NULL;
+nochild:
+        return;
+}
+
+int dsim_dphy_diag_get_reg(struct dsim_device *dsim,
+                           struct dsim_dphy_diag *diag, uint32_t *vals)
+{
+	int ret;
+	uint32_t ix, mask, val;
+
+	ret = dsim_dphy_diag_mask_from_range(diag->bit_start, diag->bit_end,
+					     &mask);
+	if (ret < 0)
+		return ret;
+
+	mutex_lock(&dsim->state_lock);
+	if (dsim->state != DSIM_STATE_HSCLKEN) {
+		ret = -ENODEV;
+		goto out;
+	}
+
+	for (ix = 0; ix < diag->num_reg; ++ix) {
+		if (diag->reg_base == REGS_DSIM_PHY_BIAS)
+			val = diag_dsim_dphy_extra_reg_read_mask(
+				dsim->id, diag->reg_offset[ix], mask);
+		else if (diag->reg_base == REGS_DSIM_PHY)
+			val = diag_dsim_dphy_reg_read_mask(
+				dsim->id, diag->reg_offset[ix], mask);
+		else {
+			pr_err("%s: invalid reg_base %u\n", __func__,
+			       diag->reg_base);
+			goto out;
+		}
+		vals[ix] = val >> diag->bit_start;
+	}
+out:
+	mutex_unlock(&dsim->state_lock);
+	return ret;
+}
+
+int dsim_dphy_diag_set_reg(struct dsim_device *dsim,
+                           struct dsim_dphy_diag *diag, uint32_t val)
+{
+	int ret;
+	u32 mask;
+
+	ret = dsim_dphy_diag_mask_from_range(diag->bit_start, diag->bit_end,
+					     &mask);
+	if (ret < 0)
+		return ret;
+
+	diag->override = true;
+	diag->user_value = (val << diag->bit_start) & mask;
+
+	mutex_lock(&dsim->state_lock);
+	if (dsim->state != DSIM_STATE_HSCLKEN)
+		goto out;
+
+	/* restart dsim to apply new config */
+	dsim_restart(dsim);
+
+out:
+	mutex_unlock(&dsim->state_lock);
+	return ret;
+}
+
+#else
+
+static void dsim_of_get_pll_diags(struct dsim_device * /* dsim */)
+{
+}
+
+#endif
+
 static void dsim_update_config_for_mode(struct dsim_reg_config *config,
 					const struct drm_display_mode *mode,
 					const struct exynos_display_mode *exynos_mode)
@@ -740,8 +927,38 @@ static const struct drm_encoder_helper_funcs dsim_encoder_helper_funcs = {
 	.atomic_check = dsim_atomic_check,
 };
 
+#ifdef CONFIG_DEBUG_FS
+
+static int dsim_encoder_late_register(struct drm_encoder *encoder)
+{
+        struct dsim_device *dsim = encoder_to_dsim(encoder);
+        dsim_diag_create_debugfs(dsim);
+        return 0;
+}
+
+static void dsim_encoder_early_unregister(struct drm_encoder *encoder)
+{
+        struct dsim_device *dsim = encoder_to_dsim(encoder);
+        dsim_diag_remove_debugfs(dsim);
+}
+
+#else
+
+static int dsim_encoder_late_register(struct drm_encoder * /* encoder */)
+{
+        return 0;
+}
+
+static void dsim_encoder_early_unregister(struct drm_encoder * /*encoder */)
+{
+}
+
+#endif
+
 static const struct drm_encoder_funcs dsim_encoder_funcs = {
 	.destroy = drm_encoder_cleanup,
+        .late_register = dsim_encoder_late_register,
+        .early_unregister = dsim_encoder_early_unregister,
 };
 
 static int dsim_add_mipi_dsi_device(struct dsim_device *dsim)
@@ -861,6 +1078,7 @@ static int dsim_parse_dt(struct dsim_device *dsim)
 	}
 
 	dsim->pll_params = dsim_of_get_clock_mode(dsim);
+	dsim_of_get_pll_diags(dsim);
 
 	return 0;
 }
@@ -1641,16 +1859,7 @@ static int dsim_set_hs_clock(struct dsim_device *dsim, unsigned int hs_clock)
 		goto out;
 
 	/* Restart dsim to apply new clock settings */
-	mutex_lock(&dsim->cmd_lock);
-	dsim_reg_stop(dsim->id, 0x1F);
-	disable_irq(dsim->irq);
-
-	dsim_reg_init(dsim->id, &dsim->config, &dsim->clk_param, true);
-	dsim_reg_start(dsim->id);
-	enable_irq(dsim->irq);
-
-	mutex_unlock(&dsim->cmd_lock);
-
+	dsim_restart(dsim);
 out:
 	mutex_unlock(&dsim->state_lock);
 

samsung/exynos_drm_dsim.h
@@ -70,6 +70,11 @@ struct dsim_device {
 	struct dsim_resources res;
 	struct clk **clks;
 	struct dsim_pll_params *pll_params;
+
+#ifdef CONFIG_DEBUG_FS
+        struct dentry *debugfs_entry;
+#endif
+
 	int irq;
 	int id;
 	spinlock_t slock;
@@ -117,4 +122,15 @@ dsim_get_decon(const struct dsim_device *dsim)
 
 void dsim_enter_ulps(struct dsim_device *dsim);
 void dsim_exit_ulps(struct dsim_device *dsim);
+
+#ifdef CONFIG_DEBUG_FS
+void dsim_diag_create_debugfs(struct dsim_device *dsim);
+void dsim_diag_remove_debugfs(struct dsim_device *dsim);
+
+int dsim_dphy_diag_get_reg(struct dsim_device *dsim,
+                           struct dsim_dphy_diag *diag, uint32_t *vals);
+int dsim_dphy_diag_set_reg(struct dsim_device *dsim,
+                           struct dsim_dphy_diag *diag, uint32_t val);
+#endif
+
 #endif /* __EXYNOS_DRM_DSI_H__ */

commit b05f2cd6b918b64498012f77e452ca464ceb3b15

GKI: compile error in diag_text_show
m->private is a void *, and not a char *.  Cast and convert.

Signed-off-by: Mark Salyzyn <salyzyn@google.com>
Fixes: bfc2beade0db ("drivers/exynos_drm: display mipi phy diag")
Change-Id: I9aa9b2055562359460b0ec98f25d8462e200ac4b

samsung/exynos_drm_debug.c
@@ -726,8 +726,10 @@ int dpu_itmon_notifier(struct notifier_block *nb, unsigned long act, void *data)
 #ifdef CONFIG_DEBUG_FS
 static int dphy_diag_text_show(struct seq_file *m, void *p)
 {
-      seq_printf(m, "%s\n", m->private);
-      return 0;
+	char *text = m->private;
+
+	seq_printf(m, "%s\n", text);
+	return 0;
 }
 
 DEFINE_SHOW_ATTRIBUTE(dphy_diag_text);