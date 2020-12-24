125e45bbd307b97e73e75ee7be09bc2ac7b36ffe
Chris Lu <luchris@google.com>
2020-12-24 10:45:40 +0800

    samsung: panel: s6e3fc3: add min brightness value
    Add the min brightness value according to the OP manual for
    brightness calculation.
    
    Bug: 176141275
    Test: Check the log for calculated brightness values
    Change-Id: I46595761fc901aa8139b924c29ba799af7bac46c
    Signed-off-by: Chris Lu <luchris@google.com>

--- samsung/panel/panel-samsung-s6e3fc3.c
+++ samsung/panel/panel-samsung-s6e3fc3.c
@@ -402,6 +402,7 @@ const struct exynos_panel_desc samsung_s6e3fc3 = {
 	.dsc_pps_len = ARRAY_SIZE(PPS_SETTING),
 	.data_lane_cnt = 4,
 	.max_brightness = 2047,
+	.min_brightness = 4,
 	.dft_brightness = 1023,
 	.brt_capability = &s6e3fc3_brightness_capability,
 	/* supported HDR format bitmask : 1(DOLBY_VISION), 2(HDR10), 3(HLG) */
