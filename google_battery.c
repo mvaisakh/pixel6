aosp-mirror/kernel_msm

google_battery: move ttf_log to google_ttf
  move the ttf_log buffer pointer to struct ttf_stats to allow logging
  of relevant information from google_ttf code.

Bug: 150526916
Test: checked logs
Signed-off-by: AleX Pelosi <apelosi@google.com>
Change-Id: I3e7f8e4cbbb7b90b30e852f2bfe72ce07bc8b3fc

drivers/power/supply/google/google_battery.c
@@ -264,7 +264,6 @@ struct batt_drv {
 	struct batt_ttf_stats ttf_stats;
 
 	/* logging */
-	struct logbuffer *ttf_log;
 	struct logbuffer *ssoc_log;
 
 	/* thermal */
@@ -2998,7 +2997,8 @@ static int gbatt_get_capacity_level(struct batt_ssoc_state *ssoc_state,
 	return capacity_level;
 }
 
-void log_ttf_estimate(const char *label, int ssoc, struct batt_drv *batt_drv)
+void bat_log_ttf_estimate(const char *label, int ssoc,
+			  struct batt_drv *batt_drv)
 {
 	int cc, err;
 	time_t res = 0;
@@ -3006,13 +3006,14 @@ void log_ttf_estimate(const char *label, int ssoc, struct batt_drv *batt_drv)
 
 	err = batt_ttf_estimate(&res, batt_drv);
 	if (err < 0) {
-		logbuffer_log(batt_drv->ttf_log, "%s ssoc=%d time=%ld err=%d",
-			(label) ? label : "", ssoc, get_boot_sec(), err);
+		logbuffer_log(batt_drv->ttf_stats.ttf_log,
+			      "%s ssoc=%d time=%ld err=%d",
+			      (label) ? label : "", ssoc, get_boot_sec(), err);
 		return;
 	}
 
 	cc = GPSY_GET_PROP(batt_drv->fg_psy, POWER_SUPPLY_PROP_CHARGE_COUNTER);
-	logbuffer_log(batt_drv->ttf_log,
+	logbuffer_log(batt_drv->ttf_stats.ttf_log,
 		      "%s ssoc=%d cc=%d time=%ld %d:%d:%d (est=%ld)",
 		      (label) ? label : "", ssoc, cc / 1000, get_boot_sec(),
 		      res / 3600, (res % 3600) / 60, (res % 3600) % 60,
@@ -3066,7 +3067,7 @@ static void google_battery_work(struct work_struct *work)
 		ssoc = ssoc_get_capacity(ssoc_state);
 		if (prev_ssoc != ssoc) {
 			if (ssoc > prev_ssoc)
-				log_ttf_estimate("SSOC", ssoc, batt_drv);
+				bat_log_ttf_estimate("SSOC", ssoc, batt_drv);
 			notify_psy_changed = true;
 		}
 
@@ -3091,7 +3092,7 @@ static void google_battery_work(struct work_struct *work)
 		/* fuel gauge triggered recharge logic. */
 		full = (ssoc == SSOC_FULL);
 		if (full && !batt_drv->batt_full)
-			log_ttf_estimate("Full", ssoc, batt_drv);
+			bat_log_ttf_estimate("Full", ssoc, batt_drv);
 		batt_drv->batt_full = full;
 	}
 
@@ -3117,7 +3118,7 @@ static void google_battery_work(struct work_struct *work)
 
 		if (fg_status != POWER_SUPPLY_STATUS_DISCHARGING &&
 		    fg_status != POWER_SUPPLY_STATUS_NOT_CHARGING) {
-			log_ttf_estimate("Start", prev_ssoc, batt_drv);
+			bat_log_ttf_estimate("Start", prev_ssoc, batt_drv);
 			batt_drv->batt_fast_update_cnt = 0;
 		} else {
 			update_interval = BATT_WORK_FAST_RETRY_MS;
@@ -3685,8 +3686,18 @@ static void google_battery_init_work(struct work_struct *work)
 	/* time to full */
 	ret = ttf_stats_init(&batt_drv->ttf_stats, batt_drv->device,
 			     batt_drv->battery_capacity);
-	if (ret < 0)
+	if (ret < 0) {
 		pr_info("time to full not available\n");
+	} else {
+		batt_drv->ttf_stats.ttf_log = debugfs_logbuffer_register("ttf");
+		if (IS_ERR(batt_drv->ttf_stats.ttf_log)) {
+			ret = PTR_ERR(batt_drv->ttf_stats.ttf_log);
+			dev_err(batt_drv->device,
+				"failed to create ttf_log, ret=%d\n", ret);
+
+			batt_drv->ttf_stats.ttf_log = NULL;
+		}
+	}
 
 	/* google_resistance  */
 	batt_res_load_data(&batt_drv->res_state, batt_drv->fg_psy);
@@ -3810,14 +3821,6 @@ static int google_battery_probe(struct platform_device *pdev)
 		batt_drv->ssoc_log = NULL;
 	}
 
-	batt_drv->ttf_log = debugfs_logbuffer_register("ttf");
-	if (IS_ERR(batt_drv->ttf_log)) {
-		ret = PTR_ERR(batt_drv->ttf_log);
-		dev_err(batt_drv->device,
-			"failed to create ttf_log, ret=%d\n", ret);
-		batt_drv->ttf_log = NULL;
-	}
-
 	/* Resistance Estimation configuration */
 	ret = of_property_read_u32(pdev->dev.of_node, "google,res-temp-hi",
 				   &batt_drv->res_state.res_temp_high);
@@ -3861,6 +3864,8 @@ static int google_battery_remove(struct platform_device *pdev)
 	struct batt_drv *batt_drv = platform_get_drvdata(pdev);
 
 	if (batt_drv) {
+		struct batt_ttf_stats *ttf_stats = &batt_drv->ttf_stats;
+
 		if (batt_drv->history)
 			gbms_storage_cleanup_device(batt_drv->history);
 		if (batt_drv->fg_psy)
@@ -3875,8 +3880,8 @@ static int google_battery_remove(struct platform_device *pdev)
 
 		if (batt_drv->ssoc_log)
 			debugfs_logbuffer_unregister(batt_drv->ssoc_log);
-		if (batt_drv->ttf_log)
-			debugfs_logbuffer_unregister(batt_drv->ttf_log);
+		if (ttf_stats->ttf_log)
+			debugfs_logbuffer_unregister(ttf_stats->ttf_log);
 		if (batt_drv->tz_dev)
 			thermal_zone_of_sensor_unregister(batt_drv->device,
 					batt_drv->tz_dev);

mvaisakh/pixel6

move the ttf_log buffer pointer to struct ttf_stats to allow logging
of relevant information from google_ttf code.
Signed-off-by: Ken Tsou <kentsou@google.com>
(cherry picked from commit 92fdccbf48d94e2470a1da650a7515e68eb03a19)

google_battery.c
@@ -291,7 +291,6 @@ struct batt_drv {
 	struct batt_ttf_stats ttf_stats;
 
 	/* logging */
-	struct logbuffer *ttf_log;
 	struct logbuffer *ssoc_log;
 
 	/* thermal */
@@ -3298,7 +3297,8 @@ static int gbatt_get_temp(struct batt_drv *batt_drv, int *temp)
 	return err;
 }
 
-static void log_ttf_estimate(const char *label, int ssoc, struct batt_drv *batt_drv)
+static void bat_log_ttf_estimate(const char *label, int ssoc,
+				 struct batt_drv *batt_drv)
 {
 	int cc, err;
 	ktime_t res = 0;
@@ -3306,13 +3306,14 @@ static void log_ttf_estimate(const char *label, int ssoc, struct batt_drv *batt_
 
 	err = batt_ttf_estimate(&res, batt_drv);
 	if (err < 0) {
-		logbuffer_log(batt_drv->ttf_log, "%s ssoc=%d time=%ld err=%d",
+		logbuffer_log(batt_drv->ttf_stats.ttf_log,
+			      "%s ssoc=%d time=%ld err=%d",
 			      (label) ? label : "", ssoc, get_boot_sec(), err);
 		return;
 	}
 
 	cc = GPSY_GET_PROP(batt_drv->fg_psy, POWER_SUPPLY_PROP_CHARGE_COUNTER);
-	logbuffer_log(batt_drv->ttf_log,
+	logbuffer_log(batt_drv->ttf_stats.ttf_log,
 		      "%s ssoc=%d cc=%d time=%ld %d:%d:%d (est=%ld)",
 		      (label) ? label : "", ssoc, cc / 1000, get_boot_sec(),
 		      res / 3600, (res % 3600) / 60, (res % 3600) % 60,
@@ -3497,7 +3498,7 @@ static void google_battery_work(struct work_struct *work)
 		ssoc = ssoc_get_capacity(ssoc_state);
 		if (prev_ssoc != ssoc) {
 			if (ssoc > prev_ssoc)
-				log_ttf_estimate("SSOC", ssoc, batt_drv);
+				bat_log_ttf_estimate("SSOC", ssoc, batt_drv);
 			notify_psy_changed = true;
 		}
 
@@ -3522,7 +3523,7 @@ static void google_battery_work(struct work_struct *work)
 		/* fuel gauge triggered recharge logic. */
 		full = (ssoc == SSOC_FULL);
 		if (full && !batt_drv->batt_full)
-			log_ttf_estimate("Full", ssoc, batt_drv);
+			bat_log_ttf_estimate("Full", ssoc, batt_drv);
 		batt_drv->batt_full = full;
 	}
 
@@ -3548,7 +3549,7 @@ static void google_battery_work(struct work_struct *work)
 
 		if (fg_status != POWER_SUPPLY_STATUS_DISCHARGING &&
 		    fg_status != POWER_SUPPLY_STATUS_NOT_CHARGING) {
-			log_ttf_estimate("Start", prev_ssoc, batt_drv);
+			bat_log_ttf_estimate("Start", prev_ssoc, batt_drv);
 			batt_drv->batt_fast_update_cnt = 0;
 		} else {
 			update_interval = BATT_WORK_FAST_RETRY_MS;
@@ -4095,8 +4096,18 @@ static void google_battery_init_work(struct work_struct *work)
 	/* time to full */
 	ret = ttf_stats_init(&batt_drv->ttf_stats, batt_drv->device,
 			     batt_drv->battery_capacity);
-	if (ret < 0)
+	if (ret < 0) {
 		pr_info("time to full not available\n");
+	} else {
+		batt_drv->ttf_stats.ttf_log = logbuffer_register("ttf");
+		if (IS_ERR(batt_drv->ttf_stats.ttf_log)) {
+			ret = PTR_ERR(batt_drv->ttf_stats.ttf_log);
+			dev_err(batt_drv->device,
+				"failed to create ttf_log, ret=%d\n", ret);
+
+			batt_drv->ttf_stats.ttf_log = NULL;
+		}
+	}
 
 	/* google_resistance  */
 	batt_res_load_data(&batt_drv->res_state, batt_drv->fg_psy);
@@ -4248,14 +4259,6 @@ static int google_battery_probe(struct platform_device *pdev)
 		batt_drv->ssoc_log = NULL;
 	}
 
-	batt_drv->ttf_log = logbuffer_register("ttf");
-	if (IS_ERR(batt_drv->ttf_log)) {
-		ret = PTR_ERR(batt_drv->ttf_log);
-		dev_err(batt_drv->device,
-			"failed to create ttf_log, ret=%d\n", ret);
-		batt_drv->ttf_log = NULL;
-	}
-
 	/* Resistance Estimation configuration */
 	ret = of_property_read_u32(pdev->dev.of_node, "google,res-temp-hi",
 				   &batt_drv->res_state.res_temp_high);
@@ -4303,8 +4306,8 @@ static int google_battery_remove(struct platform_device *pdev)
 
 	if (batt_drv->ssoc_log)
 		logbuffer_unregister(batt_drv->ssoc_log);
-	if (batt_drv->ttf_log)
-		logbuffer_unregister(batt_drv->ttf_log);
+	if (batt_drv->ttf_stats.ttf_log)
+		logbuffer_unregister(batt_drv->ttf_stats.ttf_log);
 	if (batt_drv->tz_dev)
 		thermal_zone_of_sensor_unregister(batt_drv->device,
 				batt_drv->tz_dev);