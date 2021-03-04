aosp-mirror/kernel_msm

google_battery: support for health charging aon threshold
Writing a threshold 0->99 enables HBC at the SOC% regardless of the
TTF estimate. Charging will resume with the default charging profile
when a valid (>0) deadline is met before reaching full charge.

The threshold and behavior are retained across charging sessions.
Write -1 to disable.

Bug: 159860307
Test: set threshold, check enter, reset check exit, set deadlines
Signed-off-by: AleX Pelosi <apelosi@google.com>
Change-Id: Ie4a49650263140b53311393d2e588e891e1cb938
(cherry picked from commit c87c4314f15ddaa67f56f607ac4526158d3a03ba)

drivers/power/supply/google/google_bms.h
@@ -246,6 +246,20 @@ enum gbms_stats_ac_tier_idx_t {
 	GBMS_STATS_AC_TI_ACTIVE,
 };
 
+/* health state */
+struct batt_chg_health {
+	int rest_soc;		/* entry criteria */
+	int rest_voltage;	/* entry criteria */
+	int always_on_soc;	/* entry criteria */
+
+	time_t rest_deadline;	/* full by this in seconds */
+	int rest_rate;		/* centirate once enter */
+
+	enum chg_health_state rest_state;
+	int rest_cc_max;
+	int rest_fv_uv;
+};
+
 struct gbms_charging_event {
 	union gbms_ce_adapter_details	adapter_details;
 
@@ -265,9 +279,8 @@ struct gbms_charging_event {
 	uint32_t chg_sts_delta_soc;
 
 	/* health based charging */
+	struct batt_chg_health		chg_health;
 	struct gbms_ce_tier_stats	health_stats;
-	enum chg_health_state		rest_state;
-	time_t rest_deadline;
 };
 
 #define GBMS_CCCM_LIMITS(profile, ti, vi) \

mvaisakh/pixel6

Signed-off-by: Ken Tsou <kentsou@google.com>

google_bms.h
@@ -244,6 +244,20 @@ enum gbms_stats_ac_tier_idx_t {
 	GBMS_STATS_AC_TI_ACTIVE,
 };
 
+/* health state */
+struct batt_chg_health {
+	int rest_soc;		/* entry criteria */
+	int rest_voltage;	/* entry criteria */
+	int always_on_soc;	/* entry criteria */
+
+	ktime_t rest_deadline;	/* full by this in seconds */
+	int rest_rate;		/* centirate once enter */
+
+	enum chg_health_state rest_state;
+	int rest_cc_max;
+	int rest_fv_uv;
+};
+
 struct gbms_charging_event {
 	union gbms_ce_adapter_details	adapter_details;
 
@@ -263,9 +277,8 @@ struct gbms_charging_event {
 	uint32_t chg_sts_delta_soc;
 
 	/* health based charging */
+	struct batt_chg_health		chg_health;
 	struct gbms_ce_tier_stats	health_stats;
-	enum chg_health_state		rest_state;
-	ktime_t rest_deadline;
 };
 
 #define GBMS_CCCM_LIMITS(profile, ti, vi) \