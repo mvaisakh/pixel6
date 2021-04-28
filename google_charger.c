aosp-mirror/kernel_msm

google_charger: reset whole state on abs soc
reset the whole state and not only the triggered flag when SOC drops
under trigger while disconnected. Add some logging.

Bug: 173907776
Test: trigger, drop SOC, check state
Signed-off-by: AleX Pelosi <apelosi@google.com>
Change-Id: I5ba1e1ad3da7f6679d58a0e46e6a9f07b2ee2c39

drivers/power/supply/google/google_charger.c
@@ -1376,6 +1376,12 @@ static void bd_init(struct bd_data *bd_state, struct device *dev)
 	if (!bd_state->enabled)
 		dev_warn(dev, "TEMP-DEFEND not enabled\n");
 
+	pr_info("MSC_BD: trig volt=%d,%d temp=%d,time=%d drainto=%d,%d resume=%d,%d %d,%d\n",
+		bd_state->bd_trigger_voltage, bd_state->bd_recharge_voltage,
+		bd_state->bd_trigger_temp, bd_state->bd_trigger_time,
+		bd_state->bd_drainto_soc, bd_state->bd_recharge_soc,
+		bd_state->bd_resume_abs_temp, bd_state->bd_resume_soc,
+		bd_state->bd_resume_temp, bd_state->bd_resume_time);
 }
 
 /* bd_state->triggered = 1 when charging needs to be disabled */
@@ -1412,6 +1418,7 @@ static int bd_update_stats(struct bd_data *bd_state,
 		bd_state->time_sum += now - bd_state->last_update;
 		bd_state->temp_sum += temp * (now - bd_state->last_update);
 	}
+
 	bd_state->last_voltage = vbatt;
 	bd_state->last_temp = temp;
 	bd_state->last_update = now;
@@ -1420,12 +1427,17 @@ static int bd_update_stats(struct bd_data *bd_state,
 	if (bd_state->time_sum < bd_state->bd_trigger_time)
 		return 0;
 
-	/* exit and entry criteria */
+	/* exit and entry criteria on temperature while connected */
 	temp_avg = bd_state->temp_sum / bd_state->time_sum;
-	if (triggered && temp <= bd_state->bd_resume_abs_temp)
+	if (triggered && temp <= bd_state->bd_resume_abs_temp) {
+		pr_info("MSC_BD: resume time_sum=%lld, temp_sum=%lld, temp_avg=%d\n",
+			bd_state->time_sum, bd_state->temp_sum, temp_avg);
 		bd_reset(bd_state);
-	else if (temp_avg >= bd_state->bd_trigger_temp)
+	} else if (!triggered && temp_avg >= bd_state->bd_trigger_temp) {
+		pr_info("MSC_BD: trigger time_sum=%lld, temp_sum=%lld, temp_avg=%d\n",
+			bd_state->time_sum, bd_state->temp_sum, temp_avg);
 		bd_state->triggered = 1;
+	}
 
 	return 0;
 }
@@ -1581,7 +1593,8 @@ static void bd_work(struct work_struct *work)
 	    soc < bd_state->bd_resume_soc) {
 		pr_info("MSC_BD_WORK: done soc=%d limit=%d\n",
 			soc, bd_state->bd_resume_soc);
-		bd_state->triggered = 0;
+
+		bd_reset(bd_state);
 		goto bd_rerun;
 	}
 

mvaisakh/pixel6

Signed-off-by: Ken Tsou <kentsou@google.com>
(cherry picked from commit 97203b791af616a536fa03c049c9c9cf804e6f4b)

google_charger.c
@@ -1113,6 +1113,12 @@ static void bd_init(struct bd_data *bd_state, struct device *dev)
 	if (!bd_state->enabled)
 		dev_warn(dev, "TEMP-DEFEND not enabled\n");
 
+	pr_info("MSC_BD: trig volt=%d,%d temp=%d,time=%d drainto=%d,%d resume=%d,%d %d,%d\n",
+		bd_state->bd_trigger_voltage, bd_state->bd_recharge_voltage,
+		bd_state->bd_trigger_temp, bd_state->bd_trigger_time,
+		bd_state->bd_drainto_soc, bd_state->bd_recharge_soc,
+		bd_state->bd_resume_abs_temp, bd_state->bd_resume_soc,
+		bd_state->bd_resume_temp, bd_state->bd_resume_time);
 }
 
 /* bd_state->triggered = 1 when charging needs to be disabled */
@@ -1149,6 +1155,7 @@ static int bd_update_stats(struct bd_data *bd_state,
 		bd_state->time_sum += now - bd_state->last_update;
 		bd_state->temp_sum += temp * (now - bd_state->last_update);
 	}
+
 	bd_state->last_voltage = vbatt;
 	bd_state->last_temp = temp;
 	bd_state->last_update = now;
@@ -1157,12 +1164,17 @@ static int bd_update_stats(struct bd_data *bd_state,
 	if (bd_state->time_sum < bd_state->bd_trigger_time)
 		return 0;
 
-	/* exit and entry criteria */
+	/* exit and entry criteria on temperature while connected */
 	temp_avg = bd_state->temp_sum / bd_state->time_sum;
-	if (triggered && temp <= bd_state->bd_resume_abs_temp)
+	if (triggered && temp <= bd_state->bd_resume_abs_temp) {
+		pr_info("MSC_BD: resume time_sum=%lld, temp_sum=%lld, temp_avg=%lld\n",
+			bd_state->time_sum, bd_state->temp_sum, temp_avg);
 		bd_reset(bd_state);
-	else if (temp_avg >= bd_state->bd_trigger_temp)
+	} else if (!triggered && temp_avg >= bd_state->bd_trigger_temp) {
+		pr_info("MSC_BD: trigger time_sum=%lld, temp_sum=%lld, temp_avg=%lld\n",
+			bd_state->time_sum, bd_state->temp_sum, temp_avg);
 		bd_state->triggered = 1;
+	}
 
 	return 0;
 }
@@ -1318,7 +1330,8 @@ static void bd_work(struct work_struct *work)
 	    soc < bd_state->bd_resume_soc) {
 		pr_info("MSC_BD_WORK: done soc=%d limit=%d\n",
 			soc, bd_state->bd_resume_soc);
-		bd_state->triggered = 0;
+
+		bd_reset(bd_state);
 		goto bd_rerun;
 	}
 