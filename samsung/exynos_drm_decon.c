mvaisakh/pixel6

commit 75ee1d99945716f435208b4cedab4415d7d3eca0

drm: samsung: signal commit event on frame start
Currently commit event armed right after flush and signaled during
vsync. This could incorrectly signal if vblank interrupt was pending
while arming it. In order to guarantee commit done is signaled only if
framebuffer is being fetched, we should wait until frame start interrupt
from DECON in order to signal this event.

Bug: 187993550
Signed-off-by: Adrian Salido <salidoa@google.com>
Change-Id: Iccd247d7d6390845cb65673ffeaab45410f17346

samsung/exynos_drm_decon.c
@@ -578,6 +578,42 @@ static void decon_disable_plane(struct exynos_drm_crtc *exynos_crtc,
 	decon_debug(decon, "%s -\n", __func__);
 }
 
+static void decon_send_vblank_event_locked(struct decon_device *decon)
+{
+	struct drm_crtc *crtc = &decon->crtc->base;
+	struct drm_device *dev = crtc->dev;
+
+	if (WARN_ON(!decon->event))
+		return;
+
+	spin_lock(&dev->event_lock);
+	drm_send_event_locked(dev, &decon->event->base);
+	spin_unlock(&dev->event_lock);
+
+	drm_crtc_vblank_put(crtc);
+
+	decon->event = NULL;
+}
+
+static void decon_arm_event_locked(struct exynos_drm_crtc *exynos_crtc)
+{
+	struct drm_crtc *crtc = &exynos_crtc->base;
+	struct decon_device *decon = exynos_crtc->ctx;
+	struct drm_pending_vblank_event *event = crtc->state->event;
+
+	if (!event)
+		return;
+
+	crtc->state->event = NULL;
+
+	/* in the rare case that event wasn't signaled before, signal it now */
+	if (WARN_ON(decon->event))
+		decon_send_vblank_event_locked(decon);
+
+	WARN_ON(drm_crtc_vblank_get(crtc) != 0);
+	decon->event = event;
+}
+
 static void decon_atomic_flush(struct exynos_drm_crtc *exynos_crtc,
 		struct drm_crtc_state *old_crtc_state)
 {
@@ -603,8 +639,11 @@ static void decon_atomic_flush(struct exynos_drm_crtc *exynos_crtc,
 		if (new_exynos_crtc_state->seamless_mode_changed)
 			decon_seamless_mode_set(exynos_crtc, old_crtc_state);
 
-		complete_all(&decon->framestart_done);
-		goto skip_update;
+		/* during skip update, send vblank event on next vsync instead of frame start */
+		if (!new_crtc_state->no_vblank)
+			exynos_crtc_handle_event(exynos_crtc);
+
+		return;
 	}
 
 	if (new_exynos_crtc_state->wb_type == EXYNOS_WB_CWB)
@@ -666,12 +705,9 @@ static void decon_atomic_flush(struct exynos_drm_crtc *exynos_crtc,
 
 	spin_lock_irqsave(&decon->slock, flags);
 	decon_reg_start(decon->id, &decon->config);
-	reinit_completion(&decon->framestart_done);
-	spin_unlock_irqrestore(&decon->slock, flags);
-
-skip_update:
 	if (!new_crtc_state->no_vblank)
-		exynos_crtc_handle_event(exynos_crtc);
+		decon_arm_event_locked(exynos_crtc);
+	spin_unlock_irqrestore(&decon->slock, flags);
 
 	DPU_EVENT_LOG(DPU_EVT_ATOMIC_FLUSH, decon->id, NULL);
 
@@ -1178,8 +1214,7 @@ static irqreturn_t decon_irq_handler(int irq, void *dev_data)
 
 	if (irq_sts_reg & INT_PEND_DQE_DIMMING_END) {
 		decon->keep_unmask = false;
-		if (decon->framestart_done.done &&
-				decon->config.mode.op_mode == DECON_COMMAND_MODE)
+		if (!decon->event && decon->config.mode.op_mode == DECON_COMMAND_MODE)
 			decon_reg_set_trigger(decon->id, &decon->config.mode,
 					DECON_TRIG_MASK);
 
@@ -1212,8 +1247,8 @@ static irqreturn_t decon_fs_irq_handler(int irq, void *dev_data)
 	pending_irq = decon_reg_get_fs_interrupt_and_clear(decon->id);
 
 	if (pending_irq & DPU_FRAME_START_INT_PEND) {
-		complete(&decon->framestart_done);
 		DPU_EVENT_LOG(DPU_EVT_DECON_FRAMESTART, decon->id, decon);
+		decon_send_vblank_event_locked(decon);
 		decon_debug(decon, "%s: frame start\n", __func__);
 		if (decon->config.mode.op_mode == DECON_VIDEO_MODE)
 			drm_crtc_handle_vblank(&decon->crtc->base);
@@ -1633,7 +1668,6 @@ static int decon_probe(struct platform_device *pdev)
 	decon_drvdata[decon->id] = decon;
 
 	spin_lock_init(&decon->slock);
-	init_completion(&decon->framestart_done);
 	init_waitqueue_head(&decon->framedone_wait);
 
 	decon->state = DECON_STATE_INIT;

samsung/exynos_drm_decon.h
@@ -389,7 +389,7 @@ struct decon_device {
 	struct dpu_bts			bts;
 	struct decon_debug		d;
 	struct exynos_hibernation	*hibernation;
-	struct completion		framestart_done;
+	struct drm_pending_vblank_event *event;
 	struct exynos_dqe		*dqe;
 	struct task_struct		*thread;
 	struct kthread_worker		worker;

samsung/exynos_drm_fb.c
@@ -560,8 +560,9 @@ static void exynos_atomic_commit_tail(struct drm_atomic_state *old_state)
 		}
 	}
 
-	for_each_new_crtc_in_state(old_state, crtc, new_crtc_state, i) {
+	for_each_oldnew_crtc_in_state(old_state, crtc, old_crtc_state, new_crtc_state, i) {
 		struct decon_mode *mode;
+		struct drm_crtc_commit *commit = new_crtc_state->commit;
 		int fps;
 
 		decon = crtc_to_decon(crtc);
@@ -569,20 +570,21 @@ static void exynos_atomic_commit_tail(struct drm_atomic_state *old_state)
 		if (!new_crtc_state->active)
 			continue;
 
+		if (WARN_ON(!commit))
+			continue;
+
 		fps = drm_mode_vrefresh(&new_crtc_state->mode);
 		if (old_crtc_state->active)
 			fps = min(fps, drm_mode_vrefresh(&old_crtc_state->mode));
 
-		DPU_ATRACE_BEGIN("wait_for_frame_start");
-		if (!wait_for_completion_timeout(&decon->framestart_done,
-						 fps_timeout(fps))) {
-			DPU_EVENT_LOG(DPU_EVT_FRAMESTART_TIMEOUT,
-					decon->id, NULL);
+		DPU_ATRACE_BEGIN("wait_for_crtc_flip");
+		if (!wait_for_completion_timeout(&commit->flip_done, fps_timeout(fps))) {
+			DPU_EVENT_LOG(DPU_EVT_FRAMESTART_TIMEOUT, decon->id, NULL);
 			pr_warn("decon%u framestart timeout (%d fps)\n",
 					decon->id, fps);
 			decon_dump_all(decon);
 		}
-		DPU_ATRACE_END("wait_for_frame_start");
+		DPU_ATRACE_END("wait_for_crtc_flip");
 
 		mode = &decon->config.mode;
 		if (mode->op_mode == DECON_COMMAND_MODE && !decon->keep_unmask) {

commit ee57207e52a8a547d774dae7d67790efe923f791

drm: samsung: remove warning for missing event
There are cases where DPU updates may continue without a commit
programmed. Printing warning in such cases is unnecessary, thus
removing it.

Bug: 187993550
Fixes: 75ee1d999457 ("drm: samsung: signal commit event on frame start")
Signed-off-by: Adrian Salido <salidoa@google.com>
Change-Id: I25e2b88dae9afafa37f4d6481d6a000f99e7a6f0

samsung/exynos_drm_decon.c
@@ -583,7 +583,7 @@ static void decon_send_vblank_event_locked(struct decon_device *decon)
 	struct drm_crtc *crtc = &decon->crtc->base;
 	struct drm_device *dev = crtc->dev;
 
-	if (WARN_ON(!decon->event))
+	if (!decon->event)
 		return;
 
 	spin_lock(&dev->event_lock);